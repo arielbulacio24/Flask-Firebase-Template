0fceff6bac6dc5f559ce8592e69305e7c4c6d9dc
arielbulacio <129123665+arielbulacio@users.noreply.github.com>
2024-11-05 20:33:36 -0300

    inicio repositorio

--- firebase_config.py
+++ firebase_config.py
@@ -1,4 +1,13 @@
 # This file contains the firebase configuration details.
 firebaseConfig = {
 
+
+    "apiKey": "AIzaSyBpfOHD5c-F5Yz6zoJGaCRFADwFk10uliY",
+    "authDomain": "pruebacrud-18854.firebaseapp.com",
+    "projectId": "pruebacrud-18854",
+    "storageBucket": "pruebacrud-18854.appspot.com",
+    "messagingSenderId": "225589200752",
+    "appId": "1:225589200752:web:f8ac575681edde772c31b5",
+    "measurementId": "G-SX57DJQ77T"
+
 }
--- static/firebase-config.js
+++ static/firebase-config.js
@@ -6,7 +6,17 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase
 // Your web app's Firebase configuration
 const firebaseConfig = {
 
-};
+
+    apiKey: "AIzaSyBpfOHD5c-F5Yz6zoJGaCRFADwFk10uliY",
+    authDomain: "pruebacrud-18854.firebaseapp.com",
+    projectId: "pruebacrud-18854",
+    storageBucket: "pruebacrud-18854.appspot.com",
+    messagingSenderId: "225589200752",
+    appId: "1:225589200752:web:f8ac575681edde772c31b5",
+    measurementId: "G-SX57DJQ77T"
+  };
+
+
 
   // Initialize Firebase
 const app = initializeApp(firebaseConfig);
